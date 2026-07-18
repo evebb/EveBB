d7b9aba9b016cb7ff0d88ea85de4025f7c06651c
Alan Paynter <eve.pilot@yahoo.co.uk>
2026-07-18 09:58:42 +0000

    eveBB 1.1.2-alpha
    Aligns the installed-plugins list with the upload form in the plugin
    manager.

--- install.php
+++ install.php
@@ -7,7 +7,7 @@
  */
 
 // The eveBB version this script installs
-define('FORUM_VERSION', '1.1.1-alpha');
+define('FORUM_VERSION', '1.1.2-alpha');
 
 define('FORUM_DB_REVISION', 24);
 define('FORUM_SI_REVISION', 2);
