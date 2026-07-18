ca2130e93c650695633ec4f14369c85fc6aa750f
Alan Paynter <eve.pilot@yahoo.co.uk>
2026-07-18 09:36:15 +0000

    eveBB 1.1.0-alpha
    Adds the browser-based plugin manager (Administration -> Plugins):
    upload, activate, deactivate, configure and delete plugins without
    FTP access.

--- db_update.php
+++ db_update.php
@@ -7,7 +7,7 @@
  */
 
 // The eveBB version this script updates to
-define('UPDATE_TO', '1.0.3-alpha');
+define('UPDATE_TO', '1.1.0-alpha');
 
 define('UPDATE_TO_DB_REVISION', 24);
 define('UPDATE_TO_SI_REVISION', 2);
