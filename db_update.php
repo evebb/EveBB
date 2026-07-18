d709a4af9b986635ebf6cb60f95bc037acc81f81
Alan Paynter <eve.pilot@yahoo.co.uk>
2026-07-18 10:02:10 +0000

    eveBB 1.1.3-alpha
    Moves the plugin upload section above the installed-plugins list.

--- db_update.php
+++ db_update.php
@@ -7,7 +7,7 @@
  */
 
 // The eveBB version this script updates to
-define('UPDATE_TO', '1.1.2-alpha');
+define('UPDATE_TO', '1.1.3-alpha');
 
 define('UPDATE_TO_DB_REVISION', 24);
 define('UPDATE_TO_SI_REVISION', 2);
